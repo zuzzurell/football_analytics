1235b116a5352f206ff08a73a2561ed30f2f1469
alessandro.broggio <alessandro.broggio@personio.de>
2024-02-26 16:43:25 +0100

    feat: developed first model serie a in stg and added packeges utils for dbt

---
+++ models/staging/serie_a/stg_serie_a__stagione__22_23.sql
@@ -0,0 +1,33 @@
+with load_stagione_22_23 as (
+    SELECT * FROM {{source('RAW_FOOTBALL','RAW_SERIE_A_2015_2016')}}
+),
+
+load_columns as (
+    SELECT 
+    ID::varchar as palyer_id,
+    '2022-2023' as season,
+    R::varchar as palyer_role,
+    nome::varchar as player_name,
+    squadra::varchar as palyer_team,
+    pv::integer as palyed_games,
+    mv::integer as avg_note,
+    fm::integer as avg_fantasy_note,
+    gf::integer as goal_scored,
+    gs::integer as goal_conceded,
+    rp::integer as penalty_saved,
+    rc::integer as penalty_conceded,
+    "R+"::integer as penality_scored,
+    "R-"::integer as penality_missed,
+    ass::integer as assists,
+    amm::integer as ammonitions,
+    esp::integer as explusions,
+    au::integer as autogoal
+    FROM load_stagione_22_23
+),
+--Add sk for unique player each season
+sk_unique_players as (
+    SELECT
+    {{dbt_utils.generate_surrogate_key(['palyer_id','season','palyer_team'])}} as palyer_sk,
+    *
+)
+SELECT * FROM sk_unique_players
