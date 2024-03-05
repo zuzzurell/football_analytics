50cbbd6b9a12017df59af41f2d1bc8cd2610b315
alessandro.broggio <alessandro.broggio@personio.de>
2024-03-05 20:35:24 +0100

    feat/docker_images

---
+++ test-folder/compiled/football_analytics/models/staging/serie_a/stg_serie_a__stagione__22_23.sql
@@ -0,0 +1,45 @@
+with load_stagione_22_23 as (
+    SELECT * FROM RAW_FOOTBALL.RAW_SERIE_A.RAW_SERIE_A_2022_2023
+),
+
+load_columns as (
+    SELECT 
+    ID::varchar as palyer_id,
+    '2022-2023' as season,
+    --test
+    'test_col' as test_column,
+    'test_col_2' as test_column_2,
+    'test_col_3' as test_column_3,
+    'test_col_4' as test_column_4,
+    'test_col_5' as test_column_5,
+    'test_col_6' as test_column_6,
+    'test_col_7' as test_column_7,
+    'test_col_7' as test_column_8,
+    'test_col_7' as test_column_9,
+    'test_col_7' as test_column_10,
+    R::varchar as palyer_role,
+    nome::varchar as player_name,
+    squadra::varchar as palyer_team,
+    pv::integer as palyed_games,
+    mv::integer as avg_note,
+    fm::integer as avg_fantasy_note,
+    gf::integer as goal_scored,
+    gs::integer as goal_conceded,
+    rp::integer as penalty_saved,
+    rc::integer as penalty_conceded,
+    "R+"::integer as penality_scored,
+    "R-"::integer as penality_missed,
+    ass::integer as assists,
+    amm::integer as ammonitions,
+    esp::integer as explusions,
+    au::integer as autogoal
+    FROM load_stagione_22_23
+),
+--Add sk for unique player each season
+sk_unique_players as (
+    SELECT
+    md5(cast(coalesce(cast(palyer_id as TEXT), '_dbt_utils_surrogate_key_null_') || '-' || coalesce(cast(season as TEXT), '_dbt_utils_surrogate_key_null_') || '-' || coalesce(cast(palyer_team as TEXT), '_dbt_utils_surrogate_key_null_') as TEXT)) as palyer_sk,
+    *
+    FROM load_columns
+)
+SELECT * FROM sk_unique_players
