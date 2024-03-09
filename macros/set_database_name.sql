34aadc4043ba297996e891166f56598fe8fa4123
alessandro.broggio <alessandro.broggio@personio.de>
2024-03-09 13:27:48 +0100

    new macro

--- macros/set_database_name.sql
+++ macros/set_database_name.sql
@@ -1,19 +1,9 @@
 -- Macro is used to set the database name based on the target/env
-{% macro set_database_name() %}
-  {% set default_database = target.database %}
-  {% if target.name == "dev" %}
-    DEV_DBT_FOOTBALL
-  {% elif target.name == "prod" %}
-    PROD_DBT_FOOTBALL
-  {% else %}
-    {{ default_database }}
-  {% endif %}
-{% endmacro %}
 
 {% macro generate_database_name(custom_database_name=none, node=none) -%}
 
     {%- set default_database = target.database -%}
-    {%- if custom_database_name not none -%}
+    {%- if custom_database_name is none -%}
 
         {{ default_database }}
 
